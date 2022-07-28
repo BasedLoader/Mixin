1b82d58f9c864b2777c37be82878666bfcdb812a
hydos <haydenv06@gmail.com>
2022-07-27 22:40:22 -0700

    Moderinze Mixin Structure

---
+++ src/main/java/org/spongepowered/asm/launch/MixinTransformationServiceLegacy.java
@@ -0,0 +1,11 @@
+package org.spongepowered.asm.launch;
+
+import cpw.mods.modlauncher.api.IEnvironment;
+
+public class MixinTransformationServiceLegacy extends MixinTransformationServiceAbstract {
+
+    @Override
+    public void beginScanning(IEnvironment environment) {
+    }
+
+}
